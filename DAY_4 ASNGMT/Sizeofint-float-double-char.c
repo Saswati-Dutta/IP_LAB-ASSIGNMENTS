c8d3f20b2c391ba28267c4f288514948600c24a1
Saswati_Dutta <dutta.riya2631@gmail.com>
2025-09-08 16:02:12 +0530

    Update Sizeofint-float-double-char.c

--- DAY_4 ASNGMT/Sizeofint-float-double-char.c
+++ DAY_4 ASNGMT/Sizeofint-float-double-char.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of char: %zu byte\n", sizeof(char));
+    printf("Size of int: %d bytes\n", sizeof(int));
+    printf("Size of float: %d bytes\n", sizeof(float));
+    printf("Size of double: %d bytes\n", sizeof(double));
+    printf("Size of char: %d byte\n", sizeof(char));
 
     return 0;
 }
+
